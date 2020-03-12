f4dfbb3f1f50c7cbe194d52637bd792ac4aa1ff0
Javier Contreras <javier.contreras@altitudesolutions.org>
2020-03-12 10:50:33 -0400

    now just finanzas

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,14 +12,6 @@ const app = express();
 
 // Core
 app.use(require('./users'));
-app.use(require('./personnel'));
-app.use(require('./vehicles'));
-app.use(require('./routes'));
-
-// Otros
-app.use(require('./conductores'));
-app.use(require('./servicios'));
-app.use(require('./proyectos'));
 
 // login
 app.use(require('./login'));
@@ -28,21 +20,6 @@ app.use(require('./login'));
 app.use(require('./finanzas'));
 app.use(require('./LineasDeCredito'));
 app.use(require('./PlanesDePago'));
-
-// Operador de radio
-app.use(require('./operadorRadio'));
-
-// Estacion de servicio
-app.use(require('./estacionServicio'));
-
-// Operador de base
-app.use(require('./operadorBase'));
-
-// Report generator
-app.use(require('./reportGenerator'));
-
-// Repo updater
-app.use(require('./app/updateRepo'));
 
 
 
